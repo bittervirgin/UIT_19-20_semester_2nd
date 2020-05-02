7ac5d2645940995ed3b386181d1a62379de8af98
John Trần <31305653+bittervirgin@users.noreply.github.com>
2020-05-02 12:44:11 +0700

    Update fix bugs at ring topology

--- Evolutionary_Algorithm/Particle_Swarm_Optimization/pso.py
+++ Evolutionary_Algorithm/Particle_Swarm_Optimization/pso.py
@@ -17,17 +17,22 @@ class Particle():
     def __init__(self):
         self.particle_num = 2
         self.position = np.array([])
-        for i in range(self.particle_num):
+        for _ in range(self.particle_num):
             self.position = np.append(self.position,((-1) ** (bool(random.getrandbits(1))) * random.random()))
         #print("position", self.position)
         self.pbest_position = self.position
         self.neighbour = [self.pbest_position, self.pbest_position]
         self.pbest_value = float("inf")
-        self.velocity = np.array([0,0])
+        self.velocity = np.array([])
+        for _ in range(self.particle_num):
+            self.velocity = np.append(self.velocity, 0)
     
     def _str_(self):
         print("I am at ", self.position, "meu pbest is ", self.pbest_position)
 
+    def set_particle_size(self, d):
+        self.particle_num = d
+    
     def move(self):
         self.position = self.position + self.velocity
 
@@ -49,7 +54,10 @@ class Space():
        self.n_particles = n_particles
        self.particles = []
        self.gbest_value = float("inf")
-       self.gbest_position = np.array([random.random()*50, random.random()*50])
+       self.gbest_position = np.array([random.random()*50, random.random()])
+       self.gbest_position10 = np.array([])
+       for _ in range(self.n_particles):
+            self.gbest_position10 = np.append(self.gbest_position10, random.random()*50)
        self.search_domain = domain
        self.optimal = 0
 
@@ -58,11 +66,20 @@ class Space():
             particle._str_()
 
     def rastrigin(self, particle):
-        for i in range(self.n_particles):
+        for _ in range(self.n_particles):
             sum = (particle.position[0]**2 + particle.position[1]**2) - 10*math.cos(2*math.pi*particle.position[0])*math.cos(2*math.pi*particle.position[1])
         return 10*self.n_particles + sum
         #return particle.position[0] ** 2 + particle.position[1] ** 2 + 1
     
+    def rastrigin_10(self, particle):
+        sum = 0
+        for _ in range(self.n_particles):
+            sum1 = math.cos(2*math.pi*particle.position[0])
+            for j in range(particle.particle_num - 1):
+                sum += particle.position[j+1]**2
+                sum1 = sum1* math.cos(2* math.pi*particle.position[j+1])
+            sum1 = 10*sum1
+            return 10*self.n_particles + (sum - sum1)
     def beale(self, particle):
         return (1.5 - particle.position[0] + particle.position[0]*particle.position[1])**2 + (2.25 - particle.position[0] + particle.position[0]*(particle.position[1]**2))**2 + (2.625 - particle.position[0] + particle.position[0]*(particle.position[1]**3))**2
     
@@ -73,8 +90,7 @@ class Space():
         exp = math.exp(100 - math.sqrt(((particle.position[0])**2)+(particle.position[0])**2)/math.pi)
         sum = abs(math.sin(particle.position[0])*math.sin(particle.position[1])*exp + 1)
         return -0.0001 * math.pow(sum, 0.1)
-        #print(-0.0001*(math.sin(particle.position[0])*abs(math.sin(particle.position[1])*math.exp(abs(100 - (math.sqrt(particle.position[0]**2+particle.position[1]**2)/math.pi ))+1)))**0.1)
-        #return -0.0001*math.pow(math.sin(particle.position[0])*abs(math.sin(particle.position[1])*math.exp(abs(100 - (math.sqrt(particle.position[0]**2+particle.position[1]**2)/math.pi ))+1)),0.1)
+        
     def set_pbest(self):
         for particle in self.particles:
             if self.fitness == "rastrigin":
@@ -103,15 +119,15 @@ class Space():
                 self.gbest_value = best_fitness_candidate
                 self.gbest_position = particle.position
 
-    def set_ringbest(self, i):
+    def set_ringbest(self, j):
+        i = j
         for particle in self.particles:
-            i = float("inf")
-            best_fitness = self.rastrigin(particle)
+            best_fitness = self.rastrigin_10(particle)
             if (self.gbest_value > best_fitness):
                 self.gbest_value = best_fitness
                 self.gbest_position = particle.position
             i = i + 1
-            if (i > i+3): return i
+            if (i >= j+3): return i
 
 
     def move_particles(self):
@@ -170,24 +186,51 @@ def star_topology(fitness_func):
 
 def ring_topology(fitness_func):
     random.seed(17520556)
-    domain = [-5.12, 5.12]
-    search_space = Space(1, fitness_func, n_particles, domain)
+    if fitness_func == "rastrigin":
+        search_domain = [-5.12, 5.12]
+    if fitness_func == "beale":
+        search_domain = [-4.5, 4.5]
+    if fitness_func == "himelblau":
+        search_domain = [-5., 5.]
+    if fitness_func == "cit":
+        search_domain = [-10., 10.]
+    search_space = Space(1, fitness_func, n_particles, search_domain)
     particles_vector = [Particle() for _ in range(search_space.n_particles)]
     search_space.particles = particles_vector
     search_space.print_particles()
-    
+    x_vis = np.array(search_space.gbest_position[0])
+    y_vis = np.array(search_space.gbest_position[1])
+    ####################################################
     iteration = 0
+    j = 0
     while (iteration < generation):
-        j = 0
         search_space.set_pbest()    
         j = search_space.set_ringbest(j)
-        if(search_space.gbest_position[0] < search_space.search_domain[0] or search_space.gbest_position[1] > search_space.search_domain[1]):
+        if(search_space.gbest_value == search_space.optimal):
             break
-        if (j == search_space.n_particles - 1):
+        if (j >= search_space.n_particles - 2):
             search_space.move_particles()
+            j = 0
         else:
             j = search_space.set_ringbest(j) 
+        iteration += 1
+        x_vis = np.array(search_space.gbest_position[0])
+        y_vis = np.array(search_space.gbest_position[1])
+        xmin,ymin = search_space.search_domain[0], search_space.search_domain[0]
+        xmax, ymax = search_space.search_domain[1], search_space.search_domain[1]
+        plt.axis([xmin,xmax, ymin, ymax])
+        particles_vector1 = search_space.particles
+        for particle in particles_vector1:
+        
+            x_vis = np.append(x_vis, particle.position[0])
+            y_vis = np.append(y_vis, particle.position[1])
+            #h_vis = np.append(h_vis, particle.pbest_position)
+        plt.plot(x_vis, y_vis,'ro')
+        plt.title("Generation " +  str(iteration))
+        plt.pause(0.5)
+        plt.clf()
     print("The best solution is: ", search_space.gbest_position, "in n_iteration: ", iteration)
-           
-#print(random.getrandbits(1) * random.random())
-star_topology("rastrigin")
+    print("Best fitness value", abs(search_space.gbest_value - search_space.optimal))       
+
+#star_topology("rastrigin")
+ring_topology("rastrigin")
